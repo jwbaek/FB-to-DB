0ea075ca36a6dcc558111a8c5493c5c0da6ce3a4
Jackie Baek <jackie@dropbox.com>
2014-03-29 12:46:49 -0700

    add example

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 // empty
+
+// FB.api(path, method, params, callback)
+// https://developers.facebook.com/docs/javascript/reference/FB.api
+
+function example() {
+    FB.api('/me/photos', function(response) {
+        console.log(response);
+    });
+}
+
+example()
