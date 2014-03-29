e39f6794673ee312c3acc7b4d6dce3b5c6e29845
Holly Xu <hjxu@uwaterloo.ca>
2014-03-29 14:25:22 -0700

    transitions work

--- js/script.js
+++ js/script.js
@@ -2,12 +2,10 @@
 var $container = $('[data-js="container"]');
 var $nav = $('[data-js="nav"]');
 var $nav_fb = $('[data-js="nav-fb-login"]');
-var $nav_db = $('[data-js="nav-db-login"]');
 var $nav_upload = $('[data-js="nav-album-upload"]');
 
 var $content = $('[data-js="content"]');
 var $page_fb = $('[data-js="page-fb-login"]');
-var $page_db = $('[data-js="page-db-login"]');
 var $page_upload = $('[data-js="page-album-upload"]');
 
 /* -------------------
@@ -17,25 +15,12 @@ var $page_upload = $('[data-js="page-album-upload"]');
 // Called when the Facebook login is successful
 function fbLoginComplete() {
 	$nav_fb.removeClass("active");
-	$nav_db.addClass("active");
-	$page_fb.hide();
-	$page_db.show();
-}
-
-// Called when the Dropbox login is successful
-function dbLoginComplete() {
-	$nav_db.removeClass("active");
 	$nav_upload.addClass("active");
-	$page_db.hide();
+	$page_fb.hide();
 	$page_upload.show();
 }
 
-// Upon being logged into Facebook
-// Transition from fb-login to db-login
-
-
 function login() {
-    console.log('LOGIN TEST');
     fbLoginComplete();
     FB.api('/me/photos', function (response) {
         files = [];
